ee67344f989efd06f9b70a45b1f071e95d720d87
WuLu19203 <2966603852@qq.com>
2026-04-01 16:59:39 +0800

    修正玩家动画、Camera跟随，优化了层级结构

--- Assets/Scripts/Player/CameraFollowPoint.cs
+++ Assets/Scripts/Player/CameraFollowPoint.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class CameraFollowPoint : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
+    [SerializeField] float cameraOffset = 0f;
+
+    float velocityTransform;
+
     private void Start()
     {
-         
+
+        velocityTransform = playerTransform.position.y - 1;
     }
     void Update()
     {
-        transform.position = playerTransform.position + new Vector3(0, 1, 0);
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y  - cameraOffset, 0);
     }
 }
