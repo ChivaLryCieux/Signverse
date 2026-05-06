1213b13cf90dbe4542399323936c921e3754d99a
Tempsyche <ruinature7@gmail.com>
2026-05-06 23:45:43 +0800

    feat: 加入BoltPanel与Bolt拾取物

---
+++ Assets/Scripts/UI/BoltPickupTipController.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+
+public class BoltPickupTipController : MonoBehaviour
+{
+    public static BoltPickupTipController Instance { get; private set; }
+
+    [SerializeField] private TMP_Text promptText;
+
+    private int showRequestCount;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning("场景中存在多个 BoltPickupTipController，只会使用最先初始化的一个。", this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        HideImmediate();
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    public void Show()
+    {
+        showRequestCount++;
+        SetVisible(true);
+    }
+
+    public void Hide()
+    {
+        showRequestCount = Mathf.Max(0, showRequestCount - 1);
+        if (showRequestCount == 0)
+        {
+            SetVisible(false);
+        }
+    }
+
+    public void HideImmediate()
+    {
+        showRequestCount = 0;
+        SetVisible(false);
+    }
+
+    private void SetVisible(bool visible)
+    {
+        if (promptText != null)
+        {
+            promptText.gameObject.SetActive(visible);
+        }
+    }
+}
