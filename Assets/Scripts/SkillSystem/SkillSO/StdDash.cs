179d602abe57a1fbe1d9541c078ab4478fc59d5d
Tempsyche <ruinature7@gmail.com>
2026-05-10 20:16:46 +0800

    debug冲刺技能的继承链条

--- Assets/Scripts/SkillSystem/SkillSO/StdDash.cs
+++ Assets/Scripts/SkillSystem/SkillSO/StdDash.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 namespace Skills
 {
     [CreateAssetMenu(fileName = "StdDash", menuName = "Game/Skills/Legacy/Standard Dash")]
-    public class StdDash : Skill33DDUltraDash
+    public class StdDash : Skill30StdDash
     {
-        // ===== 元数据 =====
-
-        // ===== 物理控制 =====
-        protected override bool UsesUltraDashAnimation => false;
-
-        // ===== 动画控制 =====
     }
 }
