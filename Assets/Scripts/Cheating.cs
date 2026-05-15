28c3c0bc5462da268835f7990d4308a237feff17
GotMebbudysheee <1127644819@qq.com>
2026-05-15 19:53:46 +0800

    前期修改总结

--- Assets/Scripts/Cheating.cs
+++ Assets/Scripts/Cheating.cs
@@ -7,18 +7,52 @@ using UnityEngine.SceneManagement;
 public class Cheating : MonoBehaviour
 {
     public GameObject cheat;
-    // Start is called before the first frame update
+
     void Start()
     {
         gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.yKey.wasPressedThisFrame)     cheat.gameObject.SetActive(true);
-        if(Keyboard.current.tKey.wasPressedThisFrame)     SceneManager.LoadScene(0);
-        if(Keyboard.current.gKey.wasPressedThisFrame)     SceneManager.LoadScene(1);
-        if(Keyboard.current.vKey.wasPressedThisFrame)     SceneManager.LoadScene(2);
+        if (Keyboard.current == null)
+        {
+            return;
+        }
+
+        // W + O + N + B
+        if (Keyboard.current.wKey.isPressed &&
+            Keyboard.current.oKey.isPressed &&
+            Keyboard.current.nKey.isPressed &&
+            Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            cheat.gameObject.SetActive(true);
+        }
+
+        // M + E + N + U
+        if (Keyboard.current.mKey.isPressed &&
+            Keyboard.current.eKey.isPressed &&
+            Keyboard.current.nKey.isPressed &&
+            Keyboard.current.uKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        // D + I + Y
+        if (Keyboard.current.dKey.isPressed &&
+            Keyboard.current.iKey.isPressed &&
+            Keyboard.current.yKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        // D + I + E + R
+        if (Keyboard.current.dKey.isPressed &&
+            Keyboard.current.iKey.isPressed &&
+            Keyboard.current.eKey.isPressed &&
+            Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
